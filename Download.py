89d803696c9e380edfd4b45bc53a8f44df713901
Rafael Cordeiro <fael_colt@hotmail.com>
2023-03-07 16:30:48 -0300

    Start

---
+++ Download.py
@@ -0,0 +1,23 @@
+from pytube import Youtube, Playlist
+from time import sleep
+import os
+
+def pasta_download():
+    caminho = f"{os.getenv('USER PROFILE)}\\ Downloads"
+    print(caminho)
+    
+    return caminho
+
+caminho = pasta_download()
+
+def download_unico():
+    link = input('Coloque o Link do vídeo do Youtube: ')
+    yt = Youtube(link)
+sleep(2)
+tipo = input('Você quer vídeo ou áudio? V ou A: ').lower()
+if tipo == 'v':
+    stream = yt.streams.get_by_itag(22)
+    print('Fazendo o download...')
+    sleep(2)
+    stream.download(caminho)
+    print('Download concluído...')
